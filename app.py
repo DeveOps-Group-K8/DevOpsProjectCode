d2f1b9f4978d064895b5dadee713b8faa5eda85d
Charles Ugwute Ogbonna <ogbonnacharles684@gmail.com>
2025-05-02 23:23:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create all tables
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
